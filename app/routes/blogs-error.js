457afd94b4e5e764a961a714beeea6c6773dbdae
SanthoshKM-8 <asus.santhosh.2002@gmail.com>
2024-03-05 12:10:49 +0530

    error substate updated

--- app/routes/blogs-error.js
+++ app/routes/blogs-error.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
 
-export default class BlogsErrorRoute extends Route {}
+export default Route.extend({
+    model() {
+        console.log("this is model");
+    },
+    setupController(controller, error) {
+        console.log(error.message);
+        this._super(...arguments);
+    }
+})
